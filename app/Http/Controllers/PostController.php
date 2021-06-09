b001abbe0cd79c6ce94c7b220d7a3c79d096cf5f
gtoootg0131 <80195266+gtoootg0131@users.noreply.github.com>
2021-06-09 22:48:26 +0200

    Create delete function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,4 +16,17 @@ class PostController extends Controller
     public function upload(Request $request){
         return Post::create($request -> all());
     }
+
+    // public function update(Request $request, $id){
+    //     $task=Task::findOrFail($id);
+    //     $task->update($request->all());
+    //     return $task;
+    // }
+
+    public function delete($id){
+        $task = Post::findOrFail($id);
+        $task->delete();
+        return 204;
+    }
+
 }
