255ec91ef7a2f4800e18ee75c92f88db8002f48f
gtoootg0131 <80195266+gtoootg0131@users.noreply.github.com>
2021-06-14 22:43:50 +0200

    Updated SortModal.tsx

--- public/js/index.js
+++ public/js/index.js
@@ -11866,6 +11866,7 @@ var SortModal = function SortModal() {
   };
   var initialSorterRegionsObject = [];
   var initialDisplayedCountries = [];
+  var initialCountriesCheckBox = {};
 
   var _c = react_1.useState(initialRegionsCheckBox),
       regionsCheckBox = _c[0],
@@ -11879,259 +11880,10 @@ var SortModal = function SortModal() {
       displayedCountries = _e[0],
       setDisplayedCountries = _e[1];
 
-  var _f = react_1.useState({
-    "Afghanistan": false,
-    "Åland Islands": false,
-    "Albania": false,
-    "Algeria": false,
-    "American Samoa": false,
-    "Andorra": false,
-    "Angola": false,
-    "Anguilla": false,
-    "Antarctica": false,
-    "Antigua and Barbuda": false,
-    "Argentina": false,
-    "Armenia": false,
-    "Aruba": false,
-    "Australia": false,
-    "Austria": false,
-    "Azerbaijan": false,
-    "Bahamas": false,
-    "Bahrain": false,
-    "Bangladesh": false,
-    "Barbados": false,
-    "Belarus": false,
-    "Belgium": false,
-    "Belize": false,
-    "Benin": false,
-    "Bermuda": false,
-    "Bhutan": false,
-    "Bolivia (Plurinational State of)": false,
-    "Bonaire, Sint Eustatius and Saba": false,
-    "Bosnia and Herzegovina": false,
-    "Botswana": false,
-    "Bouvet Island": false,
-    "Brazil": false,
-    "British Indian Ocean Territory": false,
-    "British Virgin Islands": false,
-    "Brunei Darussalam": false,
-    "Bulgaria": false,
-    "Burkina Faso": false,
-    "Burundi": false,
-    "Cabo Verde": false,
-    "Cambodia": false,
-    "Cameroon": false,
-    "Canada": false,
-    "Cayman Islands": false,
-    "Central African Republic": false,
-    "Chad": false,
-    "Chile": false,
-    "China": false,
-    "China, Hong Kong Special Administrative Region": false,
-    "China, Macao Special Administrative Region": false,
-    "Christmas Island": false,
-    "Cocos (Keeling) Islands": false,
-    "Colombia": false,
-    "Comoros": false,
-    "Congo": false,
-    "Cook Islands": false,
-    "Costa Rica": false,
-    "Côte d’Ivoire": false,
-    "Croatia": false,
-    "Cuba": false,
-    "Curaçao": false,
-    "Cyprus": false,
-    "Czech Republic": false,
-    "Democratic People's Republic of Korea": false,
-    "Democratic Republic of the Congo": false,
-    "Denmark": false,
-    "Djibouti": false,
-    "Dominica": false,
-    "Dominican Republic": false,
-    "Ecuador": false,
-    "Egypt": false,
-    "El Salvador": false,
-    "Equatorial Guinea": false,
-    "Eritrea": false,
-    "Estonia": false,
-    "Eswatini": false,
-    "Ethiopia": false,
-    "Falkland Islands (Malvinas)": false,
-    "Faroe Islands": false,
-    "Fiji": false,
-    "Finland": false,
-    "France": false,
-    "French Guiana": false,
-    "French Polynesia": false,
-    "French Southern Territories": false,
-    "Gabon": false,
-    "Gambia": false,
-    "Georgia": false,
-    "Germany": false,
-    "Ghana": false,
-    "Gibraltar": false,
-    "Greece": false,
-    "Greenland": false,
-    "Grenada": false,
-    "Guadeloupe": false,
-    "Guam": false,
-    "Guatemala": false,
-    "Guernsey": false,
-    "Guinea": false,
-    "Guinea-Bissau": false,
-    "Guyana": false,
-    "Haiti": false,
-    "Heard Island and McDonald Islands": false,
-    "Holy See": false,
-    "Honduras": false,
-    "Hungary": false,
-    "Iceland": false,
-    "India": false,
-    "Indonesia": false,
-    "Iran (Islamic Republic of)": false,
-    "Iraq": false,
-    "Ireland": false,
-    "Isle of Man": false,
-    "Israel": false,
-    "Italy": false,
-    "Jamaica": false,
-    "Japan": false,
-    "Jersey": false,
-    "Jordan": false,
-    "Kazakhstan": false,
-    "Kenya": false,
-    "Kiribati": false,
-    "Kuwait": false,
-    "Kyrgyzstan": false,
-    "Lao People's Democratic Republic": false,
-    "Latvia": false,
-    "Lebanon": false,
-    "Lesotho": false,
-    "Liberia": false,
-    "Libya": false,
-    "Liechtenstein": false,
-    "Lithuania": false,
-    "Luxembourg": false,
-    "Madagascar": false,
-    "Malawi": false,
-    "Malaysia": false,
-    "Maldives": false,
-    "Mali": false,
-    "Malta": false,
-    "Marshall Islands": false,
-    "Martinique": false,
-    "Mauritania": false,
-    "Mauritius": false,
-    "Mayotte": false,
-    "Mexico": false,
-    "Micronesia (Federated States of)": false,
-    "Monaco": false,
-    "Mongolia": false,
-    "Montenegro": false,
-    "Montserrat": false,
-    "Morocco": false,
-    "Mozambique": false,
-    "Myanmar": false,
-    "Namibia": false,
-    "Nauru": false,
-    "Nepal": false,
-    "Netherlands": false,
-    "New Caledonia": false,
-    "New Zealand": false,
-    "Nicaragua": false,
-    "Niger": false,
-    "Nigeria": false,
-    "Niue": false,
-    "Norfolk Island": false,
-    "North Macedonia": false,
-    "Northern Mariana Islands": false,
-    "Norway": false,
-    "Oman": false,
-    "Pakistan": false,
-    "Palau": false,
-    "Panama": false,
-    "Papua New Guinea": false,
-    "Paraguay": false,
-    "Peru": false,
-    "Philippines": false,
-    "Pitcairn": false,
-    "Poland": false,
-    "Portugal": false,
-    "Puerto Rico": false,
-    "Qatar": false,
-    "Republic of Korea": false,
-    "Republic of Moldova": false,
-    "Réunion": false,
-    "Romania": false,
-    "Russian Federation": false,
-    "Rwanda": false,
-    "Saint Barthélemy": false,
-    "Saint Helena": false,
-    "Saint Kitts and Nevis": false,
-    "Saint Lucia": false,
-    "Saint Martin (French Part)": false,
-    "Saint Pierre and Miquelon": false,
-    "Saint Vincent and the Grenadines": false,
-    "Samoa": false,
-    "San Marino": false,
-    "Sao Tome and Principe": false,
-    "Sark": false,
-    "Saudi Arabia": false,
-    "Senegal": false,
-    "Serbia": false,
-    "Seychelles": false,
-    "Sierra Leone": false,
-    "Singapore": false,
-    "Sint Maarten (Dutch part)": false,
-    "Slovakia": false,
-    "Slovenia": false,
-    "Solomon Islands": false,
-    "Somalia": false,
-    "South Africa": false,
-    "South Georgia and the South Sandwich Islands": false,
-    "South Sudan": false,
-    "Spain": false,
-    "Sri Lanka": false,
-    "State of Palestine": false,
-    "Sudan": false,
-    "Suriname": false,
-    "Svalbard and Jan Mayen Islands": false,
-    "Sweden": false,
-    "Switzerland": false,
-    "Syrian Arab Republic": false,
-    "Tajikistan": false,
-    "Thailand": false,
-    "Timor-Leste": false,
-    "Togo": false,
-    "Tokelau": false,
-    "Tonga": false,
-    "Trinidad and Tobago": false,
-    "Tunisia": false,
-    "Turkey": false,
-    "Turkmenistan": false,
-    "Turks and Caicos Islands": false,
-    "Tuvalu": false,
-    "Uganda": false,
-    "Ukraine": false,
-    "United Arab Emirates": false,
-    "United Kingdom of Great Britain and Northern Ireland": false,
-    "United Republic of Tanzania": false,
-    "United States Minor Outlying Islands": false,
-    "United States of America": false,
-    "United States Virgin Islands": false,
-    "Uruguay": false,
-    "Uzbekistan": false,
-    "Vanuatu": false,
-    "Venezuela (Bolivarian Republic of)": false,
-    "Viet Nam": false,
-    "Wallis and Futuna Islands": false,
-    "Western Sahara": false,
-    "Yemen": false,
-    "Zambia": false,
-    "Zimbabwe": false
-  }),
+  var _f = react_1.useState({}),
       countriesCheckBox = _f[0],
-      setCountriesCheckBox = _f[1];
+      setCountriesCheckBox = _f[1]; // {"Afghanistan":false,"Åland Islands":false,"Albania":false,"Algeria":false,"American Samoa":false,"Andorra":false,"Angola":false,"Anguilla":false,"Antarctica":false,"Antigua and Barbuda":false,"Argentina":false,"Armenia":false,"Aruba":false,"Australia":false,"Austria":false,"Azerbaijan":false,"Bahamas":false,"Bahrain":false,"Bangladesh":false,"Barbados":false,"Belarus":false,"Belgium":false,"Belize":false,"Benin":false,"Bermuda":false,"Bhutan":false,"Bolivia (Plurinational State of)":false,"Bonaire, Sint Eustatius and Saba":false,"Bosnia and Herzegovina":false,"Botswana":false,"Bouvet Island":false,"Brazil":false,"British Indian Ocean Territory":false,"British Virgin Islands":false,"Brunei Darussalam":false,"Bulgaria":false,"Burkina Faso":false,"Burundi":false,"Cabo Verde":false,"Cambodia":false,"Cameroon":false,"Canada":false,"Cayman Islands":false,"Central African Republic":false,"Chad":false,"Chile":false,"China":false,"China, Hong Kong Special Administrative Region":false,"China, Macao Special Administrative Region":false,"Christmas Island":false,"Cocos (Keeling) Islands":false,"Colombia":false,"Comoros":false,"Congo":false,"Cook Islands":false,"Costa Rica":false,"Côte d’Ivoire":false,"Croatia":false,"Cuba":false,"Curaçao":false,"Cyprus":false,"Czech Republic":false,"Democratic People's Republic of Korea":false,"Democratic Republic of the Congo":false,"Denmark":false,"Djibouti":false,"Dominica":false,"Dominican Republic":false,"Ecuador":false,"Egypt":false,"El Salvador":false,"Equatorial Guinea":false,"Eritrea":false,"Estonia":false,"Eswatini":false,"Ethiopia":false,"Falkland Islands (Malvinas)":false,"Faroe Islands":false,"Fiji":false,"Finland":false,"France":false,"French Guiana":false,"French Polynesia":false,"French Southern Territories":false,"Gabon":false,"Gambia":false,"Georgia":false,"Germany":false,"Ghana":false,"Gibraltar":false,"Greece":false,"Greenland":false,"Grenada":false,"Guadeloupe":false,"Guam":false,"Guatemala":false,"Guernsey":false,"Guinea":false,"Guinea-Bissau":false,"Guyana":false,"Haiti":false,"Heard Island and McDonald Islands":false,"Holy See":false,"Honduras":false,"Hungary":false,"Iceland":false,"India":false,"Indonesia":false,"Iran (Islamic Republic of)":false,"Iraq":false,"Ireland":false,"Isle of Man":false,"Israel":false,"Italy":false,"Jamaica":false,"Japan":false,"Jersey":false,"Jordan":false,"Kazakhstan":false,"Kenya":false,"Kiribati":false,"Kuwait":false,"Kyrgyzstan":false,"Lao People's Democratic Republic":false,"Latvia":false,"Lebanon":false,"Lesotho":false,"Liberia":false,"Libya":false,"Liechtenstein":false,"Lithuania":false,"Luxembourg":false,"Madagascar":false,"Malawi":false,"Malaysia":false,"Maldives":false,"Mali":false,"Malta":false,"Marshall Islands":false,"Martinique":false,"Mauritania":false,"Mauritius":false,"Mayotte":false,"Mexico":false,"Micronesia (Federated States of)":false,"Monaco":false,"Mongolia":false,"Montenegro":false,"Montserrat":false,"Morocco":false,"Mozambique":false,"Myanmar":false,"Namibia":false,"Nauru":false,"Nepal":false,"Netherlands":false,"New Caledonia":false,"New Zealand":false,"Nicaragua":false,"Niger":false,"Nigeria":false,"Niue":false,"Norfolk Island":false,"North Macedonia":false,"Northern Mariana Islands":false,"Norway":false,"Oman":false,"Pakistan":false,"Palau":false,"Panama":false,"Papua New Guinea":false,"Paraguay":false,"Peru":false,"Philippines":false,"Pitcairn":false,"Poland":false,"Portugal":false,"Puerto Rico":false,"Qatar":false,"Republic of Korea":false,"Republic of Moldova":false,"Réunion":false,"Romania":false,"Russian Federation":false,"Rwanda":false,"Saint Barthélemy":false,"Saint Helena":false,"Saint Kitts and Nevis":false,"Saint Lucia":false,"Saint Martin (French Part)":false,"Saint Pierre and Miquelon":false,"Saint Vincent and the Grenadines":false,"Samoa":false,"San Marino":false,"Sao Tome and Principe":false,"Sark":false,"Saudi Arabia":false,"Senegal":false,"Serbia":false,"Seychelles":false,"Sierra Leone":false,"Singapore":false,"Sint Maarten (Dutch part)":false,"Slovakia":false,"Slovenia":false,"Solomon Islands":false,"Somalia":false,"South Africa":false,"South Georgia and the South Sandwich Islands":false,"South Sudan":false,"Spain":false,"Sri Lanka":false,"State of Palestine":false,"Sudan":false,"Suriname":false,"Svalbard and Jan Mayen Islands":false,"Sweden":false,"Switzerland":false,"Syrian Arab Republic":false,"Tajikistan":false,"Thailand":false,"Timor-Leste":false,"Togo":false,"Tokelau":false,"Tonga":false,"Trinidad and Tobago":false,"Tunisia":false,"Turkey":false,"Turkmenistan":false,"Turks and Caicos Islands":false,"Tuvalu":false,"Uganda":false,"Ukraine":false,"United Arab Emirates":false,"United Kingdom of Great Britain and Northern Ireland":false,"United Republic of Tanzania":false,"United States Minor Outlying Islands":false,"United States of America":false,"United States Virgin Islands":false,"Uruguay":false,"Uzbekistan":false,"Vanuatu":false,"Venezuela (Bolivarian Republic of)":false,"Viet Nam":false,"Wallis and Futuna Islands":false,"Western Sahara":false,"Yemen":false,"Zambia":false,"Zimbabwe":false}
+
 
   var _g = react_1.useState([]),
       sortedCountries = _g[0],
@@ -12182,10 +11934,18 @@ var SortModal = function SortModal() {
   };
 
   var DisplayCountriesHandler = function DisplayCountriesHandler() {
+    var b = displayedCountries.forEach(function (a) {
+      var dummy = {};
+      dummy[a.country] = false; // return dummy
+
+      setCountriesCheckBox(dummy);
+      return dummy;
+    });
     var filterByRegion = countriesInPosts.filter(function (x) {
       return sortedRegions.includes(x.region);
     });
     setDisplayedCountries(filterByRegion);
+    b;
   }; // const setSortedStateHandler = () =>{
   //   setSortedState({
   //     ...sortedState,
@@ -12196,7 +11956,8 @@ var SortModal = function SortModal() {
   // }
 
 
-  var countryMap = displayedCountries.map(function (displayedCountry) {// const setSelectedCountriesHandler = (e) => {
+  var countryMap = displayedCountries.map(function (displayedCountry) {
+    // const setSelectedCountriesHandler = (e) => {
     //   const array = selectedCountries.slice()
     //   const country = showCountry.country
     //   if(!countryChecked[country]){
@@ -12209,18 +11970,12 @@ var SortModal = function SortModal() {
     //     setCountryChecked({...countryChecked, [country]:false})
     //   }
     // }
-    // return(
-    //   <div>
-    //       <input 
-    //       id="checkbox"
-    //       type="checkbox" 
-    //       name={showCountry.country} 
-    //       value={showCountry.country} 
-    //       onChange={setSelectedCountriesHandler}
-    //       // checked={countryChecked[showCountry.country]}
-    //     /> {displayedCountry.country}
-    //   </div>
-    // )     
+    return react_1["default"].createElement("div", null, react_1["default"].createElement("input", {
+      id: "checkbox",
+      type: "checkbox",
+      name: displayedCountry.country,
+      value: displayedCountry.country
+    }), " ", displayedCountry.country);
   });
   return react_1["default"].createElement("div", {
     className: sortmodal_module_scss_1["default"].sortModal,
@@ -12250,7 +12005,11 @@ var SortModal = function SortModal() {
     onClick: function onClick() {
       return console.log(displayedCountries);
     }
-  }, "displayedCountries"))); ////
+  }, "displayedCountries"), react_1["default"].createElement("button", {
+    onClick: function onClick() {
+      return console.log(countriesCheckBox);
+    }
+  }, "CountriesCheckBox"))); ////
 };
 
 exports.default = SortModal; // console.log(file);
