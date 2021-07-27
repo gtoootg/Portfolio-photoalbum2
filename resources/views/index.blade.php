f04c3183ad24a220ac520ba9219ff0dca99b97b6
gtoootg0131 <80195266+gtoootg0131@users.noreply.github.com>
2021-07-27 18:48:46 +0200

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,9 @@
         <!-- bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
         <!--           -->
+        <!-- fontawesome           -->
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+        <!--           -->
     </head>
     <body>
         <div id="app"></div>
